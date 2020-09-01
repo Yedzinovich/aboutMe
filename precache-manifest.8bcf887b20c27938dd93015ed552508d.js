5accf22cb34ecac3c2e9d53005bad7557b84f952
Inna Yedzinovich <yedzinovich@Innas-MBP.fios-router.home>
2020-09-01 14:22:53 -0400

    Updates

---
+++ precache-manifest.8bcf887b20c27938dd93015ed552508d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbba99ec16b082e228151cfedffd0c9b",
+    "url": "/aboutMe/index.html"
+  },
+  {
+    "revision": "a12fcaed8efbea6d8459",
+    "url": "/aboutMe/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "97e63636bc8fb388b7b4",
+    "url": "/aboutMe/static/js/2.ca42dc3b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/aboutMe/static/js/2.ca42dc3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a12fcaed8efbea6d8459",
+    "url": "/aboutMe/static/js/main.8dcc9947.chunk.js"
+  },
+  {
+    "revision": "21448a57e7f2b8e3c9d5",
+    "url": "/aboutMe/static/js/runtime-main.80196f05.js"
+  }
+]);
